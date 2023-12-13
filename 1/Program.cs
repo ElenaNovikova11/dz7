7e73d850380893e6d22a637db1ddafd4853cdc13
elena <princessa-len4ik@mail.ru>
2023-12-13 23:40:19 +0500

    решение первой задачи

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от M до N. Использовать рекурсию, не использовать циклы.
+
+
+string rec(int M, int N)
+{
+    if (M < N)
+        return "";
+    return rec(M - 1, N) + $"{M} ";
+}
+Console.Clear();
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(N, M));
